9465640512e2574686934f53fd1d8ec2185c3b04
NicoLongfield <lonn1001@usherbrooke.ca>
2025-05-01 16:28:46 -0400

    Fix Go server GLIBC

--- utilScripts/pack_GO_linux.sh
+++ utilScripts/pack_GO_linux.sh
@@ -1,5 +1,5 @@
 cd go_server
-go build main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o main main.go
 cd ..
 echo F | cp -p go_server/main renderer/public/server_go
 echo F | cp -p go_server/main go_executables/server_go
