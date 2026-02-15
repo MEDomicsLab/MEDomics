a8c163f2ddd609a5b93e3003e23adc6c7373c359
MahdiAll99 <mehdiwinneri01@gmail.com>
2026-02-15 11:14:53 -0500

    Fixed user path in mongodb installation

--- main/utils/installation.js
+++ main/utils/installation.js
@@ -77,6 +77,16 @@ export const checkRequirements = async () => {
   return { pythonInstalled: pythonInstalled, mongoDBInstalled: mongoDBInstalled }
 }
 
+function getHomePath() {
+  let homePath = null
+  if (process.platform === "win32") {
+    homePath = process.env.USERPROFILE
+  } else {
+    homePath = process.env.HOME
+  }
+  return homePath
+}
+
 export const installMongoDB = async () => {
   if (process.platform === "win32") {
     // Download MongoDB installer
@@ -165,11 +175,12 @@ export const installMongoDB = async () => {
       await downloadMongoDBPromise
       // Install MongoDB in the .medomics directory in the user's home directory
       ubuntuVersion = ubuntuVersion.replace(".", "")
-      console.log(`Debug starting installation of MongoDB. Command: tar -xvzf ${downloadPath} -C /home/${process.env.USER}/.medomics/ && mv /home/${process.env.USER}/.medomics/mongodb-linux-${architecture}-ubuntu${ubuntuVersion}-${mongoDBVersion} /home/${process.env.USER}/.medomics/mongodb`)
-      let command = `tar -xvzf ${downloadPath} -C /home/${process.env.USER}/.medomics/ && mv /home/${process.env.USER}/.medomics/mongodb-linux-${architecture}-ubuntu${ubuntuVersion}-${mongoDBVersion} /home/${process.env.USER}/.medomics/mongodb`
+      console.log(`Debug starting installation of MongoDB. Command: tar -xvzf ${downloadPath} -C ${userPath}/.medomics/ && mv ${userPath}/.medomics/mongodb-linux-${architecture}-ubuntu${ubuntuVersion}-${mongoDBVersion} ${userPath}/.medomics/mongodb`)
+      let userPath = getHomePath()
+      let command = `tar -xvzf ${downloadPath} -C ${userPath}/.medomics/ && mv ${userPath}/.medomics/mongodb-linux-${architecture}-ubuntu${ubuntuVersion}-${mongoDBVersion} ${userPath}/.medomics/mongodb`
       let installMongoDBPromise = exec(command)
 
-      // let installMongoDBPromise = exec(`tar -xvzf ${downloadPath} && mv mongodb-linux-${architecture}-ubuntu${ubuntuVersion}-7.0.15 /home/${process.env.USER}/.medomics/mongodb`)
+      // let installMongoDBPromise = exec(`tar -xvzf ${downloadPath} && mv mongodb-linux-${architecture}-ubuntu${ubuntuVersion}-7.0.15 ${userPath}/.medomics/mongodb`)
       execCallbacksForChildWithNotifications(installMongoDBPromise.child, "Installing MongoDB", mainWindow)
       await installMongoDBPromise
 
