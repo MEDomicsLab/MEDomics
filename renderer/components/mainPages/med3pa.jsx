c2515b55bf1d2b50826a0b12ebbed668c5ecad28
Thanh-Tri Nguyen <thanhtri_nguyen@MEDomicsMac.local>
2026-06-09 13:26:44 -0400

    added upload page

--- renderer/components/mainPages/med3pa.jsx
+++ renderer/components/mainPages/med3pa.jsx
@@ -1,13 +1,14 @@
 import React from "react"
 import ModulePage from "./moduleBasics/modulePage"
 import MED3paHelloWorldPanel from "../med3pa/med3paHelloWorldPanel"
-
+import Med3paConfigForm from "../med3pa/upload_data_page"
 const MED3paPage = ({ pageId }) => {
   return (
     <>
       <ModulePage pageId={pageId} shadow={true}>
         <h1 className="center">MED3pa Module</h1>
         <MED3paHelloWorldPanel />
+        {/* <Med3paConfigForm /> */}
       </ModulePage>
     </>
   )
